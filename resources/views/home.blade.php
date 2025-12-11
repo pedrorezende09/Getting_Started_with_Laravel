ff3682285565a807fb22fa0abd5c89c958c5ace3
Pedro Rezende <pedro_rezende09@yahoo.com.br>
2025-12-11 14:31:37 -0300

    primeiro commit do projeto laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<x-layout>
+    <x-slot:title>
+        Welcome
+    </x-slot:title>
+    
+     <div class="max-w 2xl mx-auto">
+        <div class="card bg-base-100 shadow mt-8">
+            <div class="card-body">
+                <div>
+                    <h1 class="text-3x1 font-bold">Welcome to Chriper!</h1>
+                    <p class="mt-4 text-base-content/60">This is your brand new Laravel application. Time to make it
+                        sign (or chirp)!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
